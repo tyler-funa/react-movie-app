772aeec2379dc1f0711ad3fb17fe0264cc612ee4
Tyler Funa <funa2695@gmail.com>
2022-06-23 21:21:35 -0400

    bugfix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import LandingPage from "./features/MovieSearch/pages/LandingPage";
 import { MoviesProvider } from "./features/Providers/MoviesProvider";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import FavoritesPage from './features/FavoriteMovies/pages/FavoritesPage';
 import Navbar from "./features/Navbar/Navbar";
 import Footer from "./features/Footer/Footer"
@@ -16,9 +16,10 @@ root.render(
         <Navbar />
 
         <Switch>
-          <Route exact path="/" component={LandingPage} />
+          <Route path="/react-movie-app" component={LandingPage} />
 
           <Route path="/favorites" component={FavoritesPage} />
+          <Redirect to="/react-movie-app"/>
         </Switch>
 
         <Footer/>
